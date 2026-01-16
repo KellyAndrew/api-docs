34419ab2b0a6d6a9b95742853fdc3b65163b37eb
Andrew Kelly <andrewkelly692@gmail.com>
2026-01-16 20:54:46 +0100

    Configure Astro base URL for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,20 +2,22 @@ import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
 export default defineConfig({
+  site: 'https://andrewkelly.github.io',
+  base: '/api-docs',
   integrations: [
     starlight({
-      title: 'My Docs', pagination: true,
-customCss: ['./src/styles/custom.css'],
-
+      title: 'My Docs',
       social: [
         { icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }
       ],
+      pagination: true,
+      customCss: ['./src/styles/custom.css'],
       sidebar: [
         {
           label: 'Start here',
           items: [
             { label: 'Quickstart', slug: 'quickstart' },
-			{ label: 'Authentication', slug: 'auth' },
+            { label: 'Authentication', slug: 'auth' },
             { label: 'Example Guide', slug: 'guides/example' }
           ]
         },
